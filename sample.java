beaac0a6d2892037278ea00e6e0896582ea9b5cc
pras92711 <46644201+pras92711@users.noreply.github.com>
2019-01-13 16:43:55 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,9 @@
+public class sample{
+public static void main(String[] args) {
+System.out.println("---------");
+System.out.println("prasanna");
+System.out.println("kannathasan");
+System.out.println("kannathasan");
+System.out.println("---------");
+}
+}
